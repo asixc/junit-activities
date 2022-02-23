15e21dac62d41ce62df2b476f4637c007b4d9749
josealberto.lorenzo <josealberto.lorenzo@amaris.com>
2022-02-23 18:09:03 +0100

    add assumtions

--- src/test/java/com/example/conditional/AssumptionTests.java
+++ src/test/java/com/example/conditional/AssumptionTests.java
@@ -1,6 +1,30 @@
 package com.example.conditional;
 
-public class AssumtionTests {
+import org.junit.jupiter.api.Test;
 
-    
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+public class AssumptionTests {
+
+    @Test
+    void test1() {
+        var envs = System.getenv();
+        final String jdk = System.getenv("java.home");
+        assumeTrue(jdk.contains("jdk-17"));
+
+        System.out.println("Test1 assumpt"); // No se ejcuta si no se cumple el assume
+
+    }
+
+    @Test
+    void test2() {
+        var envs = System.getenv();
+        final String jdk = System.getenv("java.home");
+        assumingThat(jdk.contains("jdk-16"),
+                () -> System.out.println("Se cumple")); //Si no se cumple no pintará nada es un if encapsulado
+
+        System.out.println("Test1 assumpt"); // Se ejecutará siempre
+
+    }
 }
