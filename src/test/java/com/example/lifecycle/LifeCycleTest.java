a9245d10d66174cee8a17fe82c81bb538e3b5504
josealberto.lorenzo <josealberto.lorenzo@amaris.com>
2022-02-23 10:41:24 +0100

    Show lifeCycle with BeforeAll & AfterEach

--- src/test/java/com/example/lifecycle/LifeCycleTest.java
+++ src/test/java/com/example/lifecycle/LifeCycleTest.java
@@ -1,2 +1,27 @@
-package com.example.lifecycle;public class LifeCycleTest {
+package com.example.lifecycle;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LifeCycleTest {
+
+    @BeforeEach
+    void setup() {
+        System.out.println("Se inicia setup");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("se ejecuta tearDown");
+    }
+    @Test
+    void test1(){
+        System.out.println("Test1");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("Test2");
+    }
 }
