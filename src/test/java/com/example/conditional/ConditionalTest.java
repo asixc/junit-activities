404adec1ab511859513ea0d5561d7de49768875a
josealberto.lorenzo <josealberto.lorenzo@amaris.com>
2022-02-23 18:09:21 +0100

    add conditional

--- src/test/java/com/example/conditional/ConditionalTest.java
+++ src/test/java/com/example/conditional/ConditionalTest.java
@@ -1,2 +1,74 @@
-package com.example.conditional;public class ConditionalTest {
+package com.example.conditional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+public class ConditionalTest {
+
+    @Test
+    void test1() {
+        System.getenv().forEach((k,v) -> System.out.println("K: " + k + ", " + v));
+
+        System.out.println("----------");
+        System.getProperties().forEach((k,v) -> System.out.println("K: " + k + ", " + v));
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_11)
+    void testJava11() {
+        System.out.println("Test with Java 11");
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    void testJava8() {
+        System.out.println("Test with Java 8");
+    }
+
+    @Test
+    @EnabledForJreRange(min=JRE.JAVA_8, max=JRE.JAVA_11)
+    void testJava8To11() {
+        System.out.println("Test with Java FROM JAVA 8 to JAVA 11");
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_17)
+    void testJava17() {
+        System.out.println("Test with Java 17");
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testForOsLinux() {
+        System.out.println("Test only for linux system");
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testForOsWindows() {
+        System.out.println("Test only for windows system");
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named ="user.language", matches = "es")
+    void testForPropertySystemSpanish() {
+        System.out.println("Test only for user.language ES");
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named ="user.language", matches = "en")
+    void testForPropertySystemEnglish() {
+        System.out.println("Test only for user.language EN");
+    }
+
+    @Test
+    @EnabledIf("isEncryptionSupported")
+    void testWithCustomConditional() {
+        System.out.println("Custom conditional test is executed");
+    }
+
+    private boolean isEncryptionSupported() {
+        return true;
+    }
+
 }
